9b240b991d378344a01ade50d656c3dca55688cb
antoniya <a.atanasova30@gmail.com>
2024-11-25 10:28:59 +0100

    Usage of Azure chat model in Controller, RAG example with custom data.

--- src/main/kotlin/com/example/rag_openai_demo/api/RecipeController.kt
+++ src/main/kotlin/com/example/rag_openai_demo/api/RecipeController.kt
@@ -1,24 +1,44 @@
 package com.example.rag_openai_demo.api
 
 import com.example.rag_openai_demo.service.VectorStoreService
-import org.springframework.ai.embedding.EmbeddingModel
+import org.springframework.ai.azure.openai.AzureOpenAiChatModel
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor
+import org.springframework.ai.vectorstore.SearchRequest
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
 class RecipeController(
-    private val vectorStoreService: VectorStoreService
+    private val vectorStoreService: VectorStoreService,
+    private val vectorStore: VectorStore,
+    private val chatModel: AzureOpenAiChatModel
 ) {
     @GetMapping("/recipe-recommendation")
     fun getRecipeEmbedding(@RequestParam prompt: String?): PromptResponse {
+        // 1. Save all of our documents
+        vectorStoreService.saveDocuments()
+
         val promptInput = prompt ?: "Give me a recipe recommendation"
-        //TODO Delete only for test purposes
-        val document = vectorStoreService.searchVectorStore(promptInput)
+
+        // 2. Configure the chat client to search into the Vector Store
+        val chatClientCustom = ChatClient.builder(chatModel)
+            .defaultAdvisors(
+                QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults())
+            ).build()
+
+        // 3. Execute the Query against the LLM
+        val response: String = chatClientCustom.prompt()
+            .user(promptInput)
+            .call()
+            .content()
+
         return PromptResponse(
             originalPrompt = promptInput,
-            searchResult = document.content
+            searchResult = response
         )
     }
 }
